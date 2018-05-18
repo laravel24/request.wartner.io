1bb978e025cf6e168838a64af441dc5c7178295f
Florian Wartner <florian@mapawa.de>
2018-05-18 09:06:37 +0200

    changes

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -8,46 +8,151 @@ class RequestController extends Controller
 {
     /**
      * @param Request $request
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
     public function getRequest(Request $request)
     {
-        return $request->all();
+        return response()->json([
+            'host' => $request->getHost(),
+            'port' => $request->getPort(),
+            'method' => $request->getMethod(),
+            'real_method' => $request->getRealMethod(),
+            'url' => $request->url(),
+            'uri' => $request->getUri(),
+            'accepts_html' => $request->acceptsHtml(),
+            'accepts_json' => $request->acceptsJson(),
+            'is_json' => $request->isJson(),
+            'is_xml' => $request->isXmlHttpRequest(),
+            'is_secure' => $request->secure(),
+            'is_no_cache' => $request->isNoCache(),
+            'locale' => $request->getLocale(),
+            'client_ip' => $request->getClientIp(),
+            'content' => $request->getContent(),
+            'content-type' => $request->getContentType(),
+            'encodings' => $request->getEncodings(),
+            'scheme' => $request->getScheme(),
+            'query' => $request->query(),
+            'files' => $request->allFiles()
+        ]);
     }
 
     /**
      * @param Request $request
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
     public function postRequest(Request $request)
     {
-        return $request->all();
+        return response()->json([
+            'host' => $request->getHost(),
+            'port' => $request->getPort(),
+            'method' => $request->getMethod(),
+            'real_method' => $request->getRealMethod(),
+            'url' => $request->url(),
+            'uri' => $request->getUri(),
+            'accepts_html' => $request->acceptsHtml(),
+            'accepts_json' => $request->acceptsJson(),
+            'is_json' => $request->isJson(),
+            'is_xml' => $request->isXmlHttpRequest(),
+            'is_secure' => $request->secure(),
+            'is_no_cache' => $request->isNoCache(),
+            'locale' => $request->getLocale(),
+            'client_ip' => $request->getClientIp(),
+            'content' => $request->getContent(),
+            'content-type' => $request->getContentType(),
+            'encodings' => $request->getEncodings(),
+            'scheme' => $request->getScheme(),
+            'query' => $request->query(),
+            'files' => $request->allFiles()
+        ]);
     }
 
     /**
      * @param Request $request
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
     public function putRequest(Request $request)
     {
-        return $request->all();
+        return response()->json([
+            'host' => $request->getHost(),
+            'port' => $request->getPort(),
+            'method' => $request->getMethod(),
+            'real_method' => $request->getRealMethod(),
+            'url' => $request->url(),
+            'uri' => $request->getUri(),
+            'accepts_html' => $request->acceptsHtml(),
+            'accepts_json' => $request->acceptsJson(),
+            'is_json' => $request->isJson(),
+            'is_xml' => $request->isXmlHttpRequest(),
+            'is_secure' => $request->secure(),
+            'is_no_cache' => $request->isNoCache(),
+            'locale' => $request->getLocale(),
+            'client_ip' => $request->getClientIp(),
+            'content' => $request->getContent(),
+            'content-type' => $request->getContentType(),
+            'encodings' => $request->getEncodings(),
+            'scheme' => $request->getScheme(),
+            'query' => $request->query(),
+            'files' => $request->allFiles()
+        ]);
     }
 
     /**
      * @param Request $request
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
     public function patchRequest(Request $request)
     {
-        return $request->all();
+        return response()->json([
+            'host' => $request->getHost(),
+            'port' => $request->getPort(),
+            'method' => $request->getMethod(),
+            'real_method' => $request->getRealMethod(),
+            'url' => $request->url(),
+            'uri' => $request->getUri(),
+            'accepts_html' => $request->acceptsHtml(),
+            'accepts_json' => $request->acceptsJson(),
+            'is_json' => $request->isJson(),
+            'is_xml' => $request->isXmlHttpRequest(),
+            'is_secure' => $request->secure(),
+            'is_no_cache' => $request->isNoCache(),
+            'locale' => $request->getLocale(),
+            'client_ip' => $request->getClientIp(),
+            'content' => $request->getContent(),
+            'content-type' => $request->getContentType(),
+            'encodings' => $request->getEncodings(),
+            'scheme' => $request->getScheme(),
+            'query' => $request->query(),
+            'files' => $request->allFiles()
+        ]);
     }
 
     /**
      * @param Request $request
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
     public function deleteRequest(Request $request)
     {
-        return $request->all();
+        return response()->json([
+            'host' => $request->getHost(),
+            'port' => $request->getPort(),
+            'method' => $request->getMethod(),
+            'real_method' => $request->getRealMethod(),
+            'url' => $request->url(),
+            'uri' => $request->getUri(),
+            'accepts_html' => $request->acceptsHtml(),
+            'accepts_json' => $request->acceptsJson(),
+            'is_json' => $request->isJson(),
+            'is_xml' => $request->isXmlHttpRequest(),
+            'is_secure' => $request->secure(),
+            'is_no_cache' => $request->isNoCache(),
+            'locale' => $request->getLocale(),
+            'client_ip' => $request->getClientIp(),
+            'content' => $request->getContent(),
+            'content-type' => $request->getContentType(),
+            'encodings' => $request->getEncodings(),
+            'scheme' => $request->getScheme(),
+            'query' => $request->query(),
+            'files' => $request->allFiles()
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'request'], function($router) {
+    $router->get('/', 'RequestController@getRequest');
+    $router->post('/', 'RequestController@postRequest');
+    $router->put('/', 'RequestController@putRequest');
+    $router->patch('/', 'RequestController@patchRequest');
+    $router->delete('/', 'RequestController@deleteRequest');
+
     $router->get('get', 'RequestController@getRequest');
     $router->post('post', 'RequestController@postRequest');
     $router->put('put', 'RequestController@putRequest');
